5b41d2819980ca09a95139f370c9b18ef9c078c4
Tonya Fletcher <tfletch4504@gmail.com>
2023-08-06 19:07:53 -0400

    social

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,8 +5,6 @@ import "./style.css";
 
 function Footer() {
   return (
-
-
     <footer className="footer fixed-bottom">
 
       <Container>
@@ -14,56 +12,13 @@ function Footer() {
           <div className="fabs inline-block">
             {social.github && <a title="Visit Github profile" rel="noopener noreferrer" target="_blank" href={social.github}><i className="fab fa-github"></i></a>}
             {social.linkedin && <a title="Visit Linkedin profile" rel="noopener noreferrer" target="_blank" href={social.linkedin}><i className="fab fa-linkedin"></i></a>}
-            {social.facebook && <a title="Visit Facebok profile" rel="noopener noreferrer" target="_blank" href={social.facebook}><i className="fab fa-facebook"></i></a>}
+            {social.facebook && <a title="Visit Facebook profile" rel="noopener noreferrer" target="_blank" href={social.facebook}><i className="fab fa-facebook"></i></a>}
             {social.instagram && <a title="Visit Instagram profile" rel="noopener noreferrer" target="_blank" href={social.instagram}><i className="fab fa-instagram"></i></a>}
-            {social.tiktok && <a title="Visit Linkedin profile" rel="noopener noreferrer" target="_blank" href={social.tiktok}><i className="fab fa-tiktok"></i></a>}
+            {social.tiktok && <a title="Visit Tiktok" rel="noopener noreferrer" target="_blank" href={social.tiktok}><i className="fab fa-tiktok"></i></a>}
             {social.resume && <a title="Download Resume" href={social.resume} download><i className="fas fa-download"></i></a>}<br />
           </div>
         </div>
       </Container>
-      {/* <div className="container">
-
-        <span>
-          
-          <ul className="nav">
-          <li className="nav-item">
-              <a title="Visit TikTok profile"
-                className="nav-link active"
-                href={social.linkedin}>
-                <i className="fab fa-linkedin" 
-                ></i></a>
-            </li>
-            <li className="nav-item">
-              <a title="Visit TikTok profile"
-                className="nav-link active"
-                href={social.facebook}>
-                <i className="fab fa-facebook-square" 
-                ></i></a>
-            </li>
-            <li className="nav-item">
-              <a title="Visit TikTok profile"
-                className="nav-link active"
-                href={social.instagram}>
-                <i className="fab fa-instagram" 
-                ></i></a>
-            </li>
-            <li className="nav-item">
-              <a title="Visit Instagram profile"
-                className="nav-link active"
-                href={social.tiktok}>
-                <i className="fab fa-tiktok" 
-                ></i></a>
-            </li>
-            <li className="nav-item">
-              <a title="View Resume"
-                className="nav-link active"
-                href={social.resume}>
-                <i className="fab fa-resume" 
-                ></i></a>
-            </li>
-          </ul>
-        </span>
-      </div> */}
 
     </footer>
   );
