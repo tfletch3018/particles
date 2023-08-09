ce19ae0d6aab900318de6f60ec19c5ce0c735660
Tonya Fletcher <tfletch4504@gmail.com>
2023-08-09 19:54:08 -0400

    update resume

--- src/social.js
+++ src/social.js
@@ -4,7 +4,7 @@ const social = {
   facebook: "https://www.facebook.com/tonya.r.fletcher/",
   instagram: "https://www.instagram.com/tfletch4504/",
   tiktok: "https://www.tiktok.com/@tfletche3018?_t=8echX7Qrkhq&_r=1",
-  resume: "https://drive.google.com/file/d/17DbwQBZecxzbgdpycnIilwh45uihriBj/view?usp=drive_link",
+  resume: "https://drive.google.com/file/d/131RLw2MfSL49xRvL0DOoKMkeIvbqPRRE/view?usp=drive_link",
 };
 
 export { social }
