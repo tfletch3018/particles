5fef31e09f7dfaebd486cb3196d1a0100bbe263c
Tonya Fletcher <tfletch4504@gmail.com>
2023-08-12 23:36:04 -0400

    second carousel is installed

---
+++ src/components/Carousel1/captions.js
@@ -0,0 +1,32 @@
+const captions = {
+    one: [
+    <div>Photo by <a href="https://unsplash.com/@vandaantje?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Danielle Suijkerbuijk</a> on <a href="https://unsplash.com/photos/a-couple-of-pink-flowers-sitting-next-to-each-other-1aW6xmxQi4Q?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+    </div>
+    ],
+    two:[
+    <div>Photo by <a href="https://unsplash.com/@eugene_golovesov?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Eugene Golovesov</a> on <a href="https://unsplash.com/photos/a-close-up-of-blue-leaves-on-a-white-background-rgSy-1XXWgg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+    </div>],
+    three:[
+    <div>Photo by <a href="https://unsplash.com/@eugene_golovesov?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Eugene Golovesov</a> on <a href="https://unsplash.com/photos/a-blurry-photo-of-yellow-flowers-with-green-leaves-NoshIxGr_OQ?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+    </div>],
+    four:[
+    <div>Photo by <a href="https://unsplash.com/@wolfgang_hasselmann?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Wolfgang Hasselmann</a> on <a href="https://unsplash.com/photos/an-infrared-image-of-a-road-and-a-tree-Du6EzCCD26s?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+    </div>],
+    five:[
+    <div>Photo by <a href="https://unsplash.com/@marekpiwnicki?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Marek Piwnicki</a> on <a href="https://unsplash.com/photos/a-purple-flower-with-green-leaves-on-a-blurry-background-sU_kKMURZbA?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+    </div>],
+    six:[
+    <div>Photo by <a href="https://unsplash.com/@wolfgang_hasselmann?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Wolfgang Hasselmann</a> on <a href="https://unsplash.com/photos/a-tree-in-a-field-with-a-blue-sky-in-the-background-V9fafZfTgck?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+    </div>],
+    seven:[
+    <div>Photo by <a href="https://unsplash.com/@wolfgang_hasselmann?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Wolfgang Hasselmann</a> on <a href="https://unsplash.com/photos/a-dirt-road-in-the-middle-of-a-snowy-forest-45QpuJ4OZfk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+    </div>],
+    eight:[
+    <div>Photo by <a href="https://unsplash.com/@wolfgang_hasselmann?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Wolfgang Hasselmann</a> on <a href="https://unsplash.com/photos/irISeGnaNFY?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+    </div>],
+    nine:[
+    <div>Photo by <a href="https://unsplash.com/@eugene_golovesov?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Eugene Golovesov</a> on <a href="https://unsplash.com/photos/tFX1nzq57ys?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+    </div>],
+}
+
+export { captions }
--- src/components/Carousel1/index.js
+++ src/components/Carousel1/index.js
@@ -1,6 +1,9 @@
 import * as React from "react";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
+import { Captions } from "yet-another-react-lightbox/plugins";
+import "yet-another-react-lightbox/plugins/captions.css"
+import { captions } from "./captions.js"
 import photo1 from "./Photos/photo1.jpg"
 import photo2 from "./Photos/photo2.jpg";
 import photo3 from "./Photos/photo3.jpg";
@@ -10,7 +13,7 @@ import photo6 from "./Photos/photo6.jpg";
 import photo7 from "./Photos/photo7.jpg";
 import photo8 from "./Photos/photo8.jpg";
 import photo9 from "./Photos/photo9.jpg";
-// import "./style.css"
+import "./style.css"
 
 function Carousel1() {
     const [open, setOpen] = React.useState(false);
@@ -24,19 +27,20 @@ function Carousel1() {
             </div>
 
             <Lightbox
+            plugins={[Captions]}
                 open={open}
                 close={() => setOpen(false)}
                 slides={
                     [
-                        { src: [photo1] },
-                        { src: [photo2] },
-                        { src: [photo3] },
-                        { src: [photo4] },
-                        { src: [photo5] },
-                        { src: [photo6] },
-                        { src: [photo7] },
-                        { src: [photo8] },
-                        { src: [photo9] },
+                        { src: [photo1], title: [captions.one], },
+                        { src: [photo2], title: [captions.two], },
+                        { src: [photo3], title: [captions.three], },
+                        { src: [photo4], title: [captions.four], },
+                        { src: [photo5], title: [captions.five], },
+                        { src: [photo6], title: [captions.six], },
+                        { src: [photo7], title: [captions.seven], },
+                        { src: [photo8], title: [captions.eight], },
+                        { src: [photo9], title: [captions.nine], },
                     ]
                 }
             />
